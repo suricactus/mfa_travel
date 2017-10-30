d9f46ddadb01a51a0c40d3b02913bcf8e4db3ecb
Ivan Ivanov <suricactus@gmail.com>
2017-10-30 09:35:57 +0200

    Add World Passport Index data

--- scripts/get_final_geojson.sh
+++ scripts/get_final_geojson.sh
@@ -1,2 +1,2 @@
 cat ../data/countries.geo.json |
-python3 ../lib/get_final_geojson.py ../data/mfa_status.csv ../data/mfa_visa_list.csv > ../data/countries_final.geo.json
+python3 ../lib/get_final_geojson.py ../data/mfa_status.csv ../data/mfa_visa_list.csv ../data/passport_index_2017.csv > ../data/countries_final.geo.json
